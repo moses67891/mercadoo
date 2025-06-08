6c56e0092a9ae42c33a2be63eab989c016ae941b
moses67891 <moses67891@gmail.com>
2025-06-08 16:46:49 +0300

    Sign up and log in added

--- mercadoo/urls.py
+++ mercadoo/urls.py
@@ -6,7 +6,6 @@ from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
