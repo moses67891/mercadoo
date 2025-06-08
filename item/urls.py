6e54f5f5609b1e342503a5dc138d0647695c5a08
moses67891 <moses67891@gmail.com>
2025-06-08 18:20:41 +0300

    added ability to delete and edit items

--- item/urls.py
+++ item/urls.py
@@ -6,4 +6,6 @@ app_name = 'item'
 urlpatterns = [
     path('new/', new, name='new' ),
     path('<int:pk>/', detail, name='detail'),
+    path('<int:pk>/delete/', delete, name='delete'),
+    path('<int:pk>/dit/', edit, name='edit'),
 ]
